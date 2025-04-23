2b59ff0eb390e528b4fea2bc72a9d9b042a2c997
lkuca <luca.gluhhov@gmail.com>
2025-04-23 13:05:28 +0300

    added shooting

---
+++ Assets/Projectile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/scripts/PlayerWeaponManager.cs
+++ Assets/scripts/PlayerWeaponManager.cs
@@ -4,41 +4,58 @@ using UnityEngine;
 
 public class PlayerWeaponManager : MonoBehaviour
 {
-    public string curWeaponType; 
+    public string curWeaponType;
     public bool inTrigger = false;
+    public Transform firePoint; // Точка появления пули
+    public GameObject projectilePrefab; // Префаб пули
     Animator animator;
-    //public static bool isarmedAnim;
 
-
-    void Start () {
+    void Start()
+    {
         animator = GetComponent<Animator>();
-
     }
 
-
-    public void Update () {
+    public void Update()
+    {
         WeaponManager();
+        HandleShooting();
     }
 
     void WeaponManager()
     {
-        if(Input.GetMouseButtonDown(1) && !inTrigger)
+        if (Input.GetMouseButtonDown(1) && !inTrigger)
         {
             dropWeapon(curWeaponType);
             animator.SetBool("isarmedanim", false);
             animator.CrossFade("idle_animation", 0.2f, 0);
         }
-        
     }
+
+    void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0) && curWeaponType != "Null")
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (projectilePrefab != null && firePoint != null)
+        {
+            Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        }
+        else
+        {
+            Debug.LogWarning("Projectile Prefab или FirePoint не установлены!");
+        }
+    }
+
     public void dropWeapon(string weapon)
     {
-        if(curWeaponType != "Null")
+        if (curWeaponType != "Null")
         {
-            //isarmedAnim = true;
-            //Debug.Log("net naturala otvet");
-            //animator.SetBool("isarmedanim", true);
-            //animator.CrossFade("armed", 0.2f, 0);
-            Vector3 newpose= new Vector3(transform.position.x, transform.position.y, 0);
+            Vector3 newpose = new Vector3(transform.position.x, transform.position.y, 0);
             Instantiate(Resources.Load("Prefabs/Items/" + weapon), newpose, Quaternion.identity);
             if (!inTrigger)
                 curWeaponType = "Null";
@@ -48,20 +65,15 @@ public class PlayerWeaponManager : MonoBehaviour
             Debug.Log("weapon is null");
         }
     }
+
     public void setcurweapon(string weapon)
     {
-
-        curWeaponType= weapon;
-       
-
-        
+        curWeaponType = weapon;
     }
 
     public void playAnimwhenWeaponishold()
     {
-        Debug.Log("oi");
         animator.SetBool("isarmedanim", true);
         animator.CrossFade("armed", 0.2f, 0);
-
     }
 }
